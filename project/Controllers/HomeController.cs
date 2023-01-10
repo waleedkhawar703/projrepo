6290a7d95dde4678b5f0845647421590b20f6e9b
HP <HP@DESKTOP-T86DS7F>
2023-01-10 22:01:27 +0500

    Add project files.

---
+++ project/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project.Models;
+using System.Diagnostics;
+
+namespace project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult contact()
+        {
+            return View();
+        }
+
+        public IActionResult about()
+        {
+            return View();
+        }
+
+        public IActionResult blogs()
+        {
+            return View();
+        }
+
+        public IActionResult products()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ project/wwwroot/js/site.js
@@ -0,0 +1,50 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// contact
+function validateForm() {
+    var name = document.forms["myForm"]["name"];
+    var email = document.forms["myForm"]["email"];
+    var message = document.forms["myForm"]["message"];
+
+    if (name.value == "") {
+        document.getElementById('errorname').innerHTML = "Please enter a valid name";
+        name.focus();
+        return false;
+    } else {
+        document.getElementById('errorname').innerHTML = "";
+    }
+
+    if (email.value == "") {
+        document.getElementById('erroremail').innerHTML = "Please enter a valid email address";
+        email.focus();
+        return false;
+    } else {
+        document.getElementById('erroremail').innerHTML = "";
+    }
+
+    if (email.value.indexOf("@", 0) < 0) {
+        document.getElementById('erroremail').innerHTML = "Please enter a valid email address";
+        email.focus();
+        return false;
+    }
+
+    if (email.value.indexOf(".", 0) < 0) {
+        document.getElementById('erroremail').innerHTML = "Please enter a valid email address";
+        email.focus();
+        return false;
+    }
+
+    if (message.value == "") {
+        document.getElementById('errormsg').innerHTML = "Please enter a valid message";
+        message.focus();
+        return false;
+    } else {
+        document.getElementById('errormsg').innerHTML = "";
+    }
+
+    return true;
+}
+
